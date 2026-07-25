a2bf1a2a6bef249f08dab6b690a072ef5d67e6dc
Xodder01 <lakhinder9876@gmail.com>
2026-07-25 21:24:48 +0530

    login-logout added

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -2,7 +2,7 @@ import { sendWelcomeEmail } from "../emails/emailHandlers.js";
 import { generateToken } from "../lib/utils.js";
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
-import {ENV} from '../lib/env.js'
+import { ENV } from "../lib/env.js";
 
 export const signup = async (req, res) => {
   const { fullName, email, password } = req.body;
@@ -61,4 +61,36 @@ export const signup = async (req, res) => {
     console.log("Error in signup controller: ", error);
     res.status(500).json({ message: "Internal server error" });
   }
+};
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+
+    if (!user) return res.status(400).json({ message: "Invalid credentials" }); //never tell your user that which one is incorrect.
+
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordCorrect)
+      return res.status(400).json({ message: "Invalid credentials" });
+
+    generateToken(user._id, res);
+
+    res.status(201).json({
+      _id: User._id,
+      fullName: User.fullName,
+      email: User.email,
+      profilePic: User.profilePic,
+    });
+  } catch (error) {
+    console.error("Error in the login controller", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const logout = (_, res) => {
+  res.cookie("jwt", "", { maxAge: 0 });
+  res.status(200).json({ message: "Logged out successfully" });
 };
